b6d0fb649114a21856a00e9a99dfb32d62efa13d
soniya <ssinghal22@yahoo.com>
2022-07-11 11:33:14 -0700

    changes to file TestRunner

--- src/test/java/support/TestRunner.java
+++ src/test/java/support/TestRunner.java
@@ -5,13 +5,13 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-//@RunWith(Cucumber.class)
-//@CucumberOptions(
-//        features = "src/test/resources/features",
-//        glue = {"definitions", "support"},
-//        plugin = {"pretty", "html:target/cucumber", "json:target/cucumber/report.json"}
-//)
-//
-//public class TestRunner {
-//}
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"definitions", "support"},
+        plugin = {"pretty", "html:target/cucumber", "json:target/cucumber/report.json"}
+)
+
+public class TestRunner {
+}
 
