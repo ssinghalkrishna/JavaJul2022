d28d2ae460c02fd17126ca4006116359caf1658d
soniya <ssinghal22@yahoo.com>
2022-07-28 16:44:40 -0700

    add 1 file

---
+++ src/test/java/javaTests/UniqueDigitsInNumber.java
@@ -0,0 +1,42 @@
+package javaTests;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueDigitsInNumber {
+    // Print all numbers from 100 to 999 inclusive, which does not have duplicate digits.
+    //102, 103 are fine
+    // not 100, 101, 111, 122
+    public static void main(String[] args) {
+        printNonDuplicateNums(100, 120);
+    }
+
+    private static void printNonDuplicateNums(int low, int high) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = low; i <= high; i++) {
+            if (isNotDupDigits(i)) {
+                list.add(i);
+            }
+        }
+        System.out.println();
+        System.out.println(list);
+    }
+
+    private static boolean isNotDupDigits(int num) {
+        System.out.print(num + " ");
+        Set<Integer> set = new HashSet<Integer>();
+
+        while (num > 0) { //101, 10, 1
+            int digit = num % 10; //1, 0
+            if (set.contains(digit)) {
+                return false;
+            } else {
+                set.add(digit);
+            }
+            num = num / 10;//10, 1
+        }
+        return true;
+    }
+
+}
